df456349decac0e973552465171d4324db77bb19
zoe <zhedstrom@greenwichacademy.org>
2021-02-09 18:00:42 -0500

    pressfitbox

---
+++ pressfit/sketch.js
@@ -0,0 +1,45 @@
+let img;
+
+function preload (){
+ 
+  
+  img = loadImage ('bee.png');
+        
+ 
+}
+  
+
+function setup() {
+  createCanvas(windowWidth, windowWidth);
+   frameRate (0.5);
+  noFill
+  noStroke();
+ 
+
+}
+
+function draw() {
+  
+  var num = 5
+  var sideLen = windowWidth/num;
+  
+  translate(60, 60);
+  for (var y = 0; y < windowWidth; y = y+sideLen)
+  for (var x = 0; x < windowWidth; x = x+sideLen) {
+    
+    
+  quad (x, y,
+    x+sideLen, y,
+    x +sideLen, y+sideLen,
+    x, y+sideLen);
+    
+        image (img, x, y);
+    
+    
+    
+  }
+
+  
+  
+  
+}
