8ba2ebf3ff7cb0e071c810232e4bca9b0bc095fb
zoe <zhedstrom@greenwichacademy.org>
2021-02-18 15:55:55 -0500

    newlizard

--- lizardtolizard/sketch.js
+++ lizardtolizard/sketch.js
@@ -1,7 +1,7 @@
 let img;
 
 function preload (){
-img = loadImage ('GL.gif');
+img = loadImage ('gombeylizard.gif');
 }
 
 
@@ -9,6 +9,7 @@ img = loadImage ('GL.gif');
 
 function setup() {
   createCanvas(windowWidth, windowWidth);
+  
 strokeWeight (0);
 noStroke;
  
@@ -20,7 +21,7 @@ function draw() {
   var num = 1
 var sideLen = windowWidth/num
   
-  translate(60, 60);
+  translate(10, 10);
   for (var y = 0; y < windowWidth; y = y+sideLen)
   for (var x = 0; x < windowWidth; x = x+sideLen) {
     
@@ -30,7 +31,7 @@ var sideLen = windowWidth/num
     x +sideLen, y+sideLen,
     x, y+sideLen);
     
-   image (img, 200,300
+   image (img, 300,200
          );
   }
 
