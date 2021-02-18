f8e5e54f8c4e02c9315b2d971dd2a4be39865118
zoe <zhedstrom@greenwichacademy.org>
2021-02-18 16:14:12 -0500

    leapinlizard

---
+++ lizardtolizard/tree/sketch.js
@@ -0,0 +1,43 @@
+let img;
+
+function preload (){
+img = loadImage ('tree.gif');
+}
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+strokeWeight (0);
+noStroke;
+ 
+
+}
+
+function draw() {
+  
+  var num = 1
+var sideLen = windowWidth/num
+  
+  translate(10, 10);
+  for (var y = 0; y < windowWidth; y = y+sideLen)
+  for (var x = 0; x < windowWidth; x = x+sideLen) {
+    
+    
+  quad (x, y,
+    x+sideLen, y,
+    x +sideLen, y+sideLen,
+    x, y+sideLen);
+    
+   image (img, 300,200
+         );
+  }
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  
+  
+}
+}
