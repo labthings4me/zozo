fa8874617472a4ce7f5f4779915818a557b1ec1e
labthings4me <zoehedstrom@yahoo.com>
2022-01-10 19:43:53 -0500

    new

--- redomovinglizard/sketch.js
+++ redomovinglizard/sketch.js
@@ -1,54 +1,48 @@
-let pics;
+
 
 function preload (){
-bermudiana= loadImage ('images/bermudiana.png');
-hat = loadImage ('images/hat.png');
-lizard= loadImage ('images/lizard.png');
- 
-  
-  
- 
-}
-  
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-   frameRate (0.5);
-  noFill ;
-  noStroke();
- 
+  bermudiana = loadImage ('bermudiana.png');
+  hat = loadImage ('hat.png');
+  lizard = loadImage ('lizard.png');
 
 }
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+    frameRate (0.5);
+    fill("white");
+    noStroke ();
+  }
+
 
 function draw() {
-  ;
-  
-  var num = 5
-  var sideLen = windowWidth/num;
-  
-  translate(60, 60);
-  for (var y = 0; y < windowWidth; y = y+sideLen)
-  for (var x = 0; x < windowWidth; x = x+sideLen) {
-    
-    
-  quad (x, y,
-    x+sideLen, y,
-    x +sideLen, y+sideLen,
-    x, y+sideLen);
-    
-         var pics =  [bermudiana, hat, lizard,];
-       
-      image (pics [int (random (0,3))], x, y );
-    
-       
-    
-    
-    
-  }
-  
-//noLoop ();
-  
-  
-  
-}
 
+
+ var num = 4 ;
+
+  var sideLen = windowWidth/num; // x length
+
+
+  for ( var y= 0; y < windowHeight; y = y+sideLen) {
+
+    for ( var x= 0; x < windowWidth; x = x + sideLen) {//row across x
+
+   quad (x, y,
+        x + sideLen, y,
+        x + sideLen, y + sideLen,
+        x , y + sideLen);
+
+    var pics = [bermudiana,hat, lizard];
+    image (pics [int (random (0,3
+                             ))], x,y);
+
+
+    }
+
+  function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+
+  }
+
+  }
+
+}
