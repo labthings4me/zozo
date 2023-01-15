1747e4386ceeb0288aa209a96f635738295d5715
labthings4me <zoehedstrom@yahoo.com>
2023-01-14 20:19:42 -0500

    update

---
+++ NewLab/sketch.js
@@ -0,0 +1,56 @@
+let pics;
+
+function preload (){
+bermudiana= loadImage ('images/bermudiana.png');
+hat = loadImage ('images/hat.png');
+lizard= loadImage ('images/lizard.png');
+
+
+
+
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowWidth);
+   frameRate (0.5);
+  background ('white');
+  noStroke();
+
+
+}
+
+function draw() {
+  background ('white');
+
+  var num = 5
+  var sideLen = windowWidth/num;
+
+    translate (-150, -150); // gives design a bleed effect
+
+    for ( var y = 0; y < 2 * windowWidth; y = y + sideLen) {
+
+        for ( var x = 0; x < 2 * windowWidth; x = x + sideLen) {//row across x
+
+
+
+
+
+         var pics =  [bermudiana, hat, lizard,];
+
+      image (pics [int (random (0,3))], x, y );
+    }
+
+      function windowResized() {
+        resizeCanvas(windowWidth, windowHeight);
+
+
+
+  }
+
+//noLoop ();
+
+
+
+}
+}
