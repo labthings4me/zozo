13379ed7834a98115e8645065d8448f28273bb84
labthings4me <zoehedstrom@yahoo.com>
2023-01-14 19:43:25 -0500

    update

--- movinglizard_copy/sketch.js
+++ movinglizard_copy/sketch.js
@@ -27,23 +27,22 @@ function draw() {
   var sideLen = windowWidth/num;
 
     translate (-150, -150); // gives design a bleed effect
-    
+
     for ( var y = 0; y < 2 * windowWidth; y = y + sideLen) {
 
         for ( var x = 0; x < 2 * windowWidth; x = x + sideLen) {//row across x
 
 
 
-  quad (x, y,
-    x+sideLen, y,
-    x +sideLen, y+sideLen,
-    x, y+sideLen);
+  
 
          var pics =  [bermudiana, hat, lizard,];
 
       image (pics [int (random (0,3))], x, y );
+    }
 
-
+      function windowResized() {
+        resizeCanvas(windowWidth, windowHeight);
 
 
 
@@ -53,4 +52,5 @@ function draw() {
 
 
 
+}
 }
